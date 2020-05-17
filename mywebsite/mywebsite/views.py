e93e5d402b203a84a94871e3913e6ef87282ab21
John Pendenque <pendenquejohn@gmail.com>
2020-05-17 11:43:26 +0200

    Updated hero page

--- mywebsite/mywebsite/views.py
+++ mywebsite/mywebsite/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 
 class HeroView(TemplateView):
     template_name = 'home/hero.html'
+
+    def post(self, request, **kwargs):
+        return JsonResponse({'success': 'success'})
