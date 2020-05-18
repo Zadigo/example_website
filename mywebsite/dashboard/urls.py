9e378ed5961dff213937c6003107c3fc7e2641bb
John Pendenque <pendenquejohn@gmail.com>
2020-05-18 18:42:37 +0200

    Improvements on dashboard

--- mywebsite/dashboard/urls.py
+++ mywebsite/dashboard/urls.py
@@ -5,12 +5,14 @@ from dashboard import views
 urlpatterns = [
     # url(r'^settings/$', views.SettingsView.as_view(), name='settings'),
     # url(r'^login/$', views.DashboardLoginView.as_view(), name='dashboard_login'),
-    # url(r'^create/$', views.CreateNewView.as_view(), name='create'),
     # url(r'^list/cards/$', views.ListItemsCardsView.as_view(), name='list_items_cards'),
     
-    # url(r'list/(?P<pk>\d+)/update/$', views.UpdateItemView.as_view(), name='update'),
-    # url(r'list/users/(?P<pk>\d+)$', views.UserDetailsView.as_view(), name='user_details'),
+    url(r'list/(?P<pk>\d+)/update/$', views.UpdateItemView.as_view(), name='dashboard_update'),
+    url(r'list/users/(?P<pk>\d+)$', views.UserDetailsView.as_view(), name='user_details'),
     url(r'list/(?P<pk>\d+)$', views.ItemDetailsView.as_view(), name='dashboard_details'),
+
+    url(r'^create/$', views.CreateNewView.as_view(), name='dashboard_create'),
+    url(r'list/users$', views.ListUsersView.as_view(), name='dashboard_users'),
     url(r'list/$', views.ListItemsView.as_view(), name='dashboard_products'),
     url(r'^index/$', views.IndexView.as_view(), name='index')
 ]
--- mywebsite/dashboard/views.py
+++ mywebsite/dashboard/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 from dashboard import models
+try:
+    from accounts import models as accounts_models
+except:
+    from django.contrib.auth import get_user_model
+    USER = get_user_model()
 
 class IndexView(generic.View):
     def get(self, request, *args, **kwargs):
@@ -22,3 +27,23 @@ class ItemDetailsView(generic.DetailView):
     queryset = models.DashboardSetting.objects.all()
     template_name = 'pages/details.html'
     context_object_name = 'celebrity'
+
+class ListUsersView(generic.ListView):
+    model = accounts_models.MyUser
+    queryset = accounts_models.MyUser.objects.all()
+    template_name = 'pages/profiles.html'
+    context_object_name = 'users'
+
+class UserDetailsView(generic.DetailView):
+    model = accounts_models.MyUser
+    queryset = accounts_models.MyUser.objects.all()
+    template_name = 'pages/profile.html'
+    context_object_name = 'user'
+
+class CreateNewView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pages/create.html')
+
+class UpdateItemView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pages/create.html')
