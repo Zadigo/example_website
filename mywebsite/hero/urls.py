90dc4d7a72d7f2d308d498c8fe471acec1f87a4b
John Pendenque <pendenquejohn@gmail.com>
2020-11-08 22:44:40 +0100

    Updated contact form + Refactoring

--- mywebsite/hero/urls.py
+++ mywebsite/hero/urls.py
@@ -5,5 +5,6 @@ from hero import views
 app_name = 'hero'
 
 urlpatterns = [
+    url(r'^send-message$', views.send_message, name='contact'),
     url(r'^$', views.HeroView.as_view(), name='home')
 ]
--- mywebsite/hero/views.py
+++ mywebsite/hero/views.py
@@ -1,12 +1,26 @@
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST
 from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
+from django.core.mail import send_mail
 
 
-@method_decorator(cache_page(240 * 60), name='dispatch')
+# @method_decorator(cache_page(240 * 60), name='dispatch')
 class HeroView(TemplateView):
     template_name = 'pages/home.html'
 
-    def post(self, request, **kwargs):
-        return JsonResponse({'success': 'success'})
+
+@require_POST
+def send_message(request, **kwargs):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    message = request.POST.get('message')
+
+    send_mail(
+        '',
+        message=message,
+        from_email='',
+        recipient_list=[email]
+    )
+    return JsonResponse(data={})
