34c3a677e69e9f00ed436a0ac121fe5151d64dfb
John Pendenque <pendenquejohn@gmail.com>
2020-11-09 23:50:28 +0100

    New dashboard version

--- mywebsite/dashboard/admin.py
+++ mywebsite/dashboard/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from dashboard import models
 
-@admin.register(models.DashboardSetting)
-class DashboardSettingsAdmin(admin.ModelAdmin):
-    list_display = ['myuser', 'dark_mode']
+# Register your models here.
