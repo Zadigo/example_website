aeb0bbd2c011ae0815f620b4fc38ac8e45926870
John Pendenque <pendenquejohn@gmail.com>
2020-11-11 12:39:12 +0100

    Improved bar chart

--- mywebsite/dashboard/views.py
+++ mywebsite/dashboard/views.py
@@ -14,6 +14,19 @@ from requests import get as get_request
 
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = forms.TestForm
+
+        charts = {
+            'barchart': {
+                'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                'data': [12, 19, 3, 5, 2, 3]
+            }
+        }
+        context['charts'] = charts
+        return context
     
 
 class ProductsView(TemplateView):
