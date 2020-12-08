d4ab997aabab07145e94716e05887fd210abfa77
John Pendenque <pendenquejohn@gmail.com>
2020-12-09 00:26:05 +0100

    Hero app refactoring

--- mywebsite/hero/views.py
+++ mywebsite/hero/views.py
@@ -1,7 +1,7 @@
+from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView
-from django.core.mail import send_mail
 
 
 class HeroView(TemplateView):
