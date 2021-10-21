9296345326d470dc5cac4c8b9b0832df0d34a2fc
Zadigo <pendenquejohn@gmail.com>
2021-10-21 20:04:08 +0200

    Updated API app

---
+++ api/__init__.py
@@ -0,0 +1,6 @@
+from django.apps import apps as django_apps
+
+def register_graphql_models(custom_site):
+    app = django_apps.get_app_config('graphql_auth')
+    for _, model in app.models.items():
+        custom_site.register(model)
--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
+from accounts import get_custom_site
 from django.contrib import admin
+from graphql_jwt.refresh_token.models import RefreshToken
+# from rest_framework.authtoken.models import Token
+from api import register_graphql_models
 
-# Register your models here.
+site = get_custom_site()
+
+
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created')
+    fields = ('user',)
+    ordering = ('-created',)
+
+site.register(RefreshToken)
+# site.register(Token, TokenAdmin)
+register_graphql_models(site)
