5d92c83350a50e131792c1cff38e1061683cea63
Zadigo <pendenquejohn@gmail.com>
2021-10-21 19:19:51 +0200

    Added tests

--- legal/tests.py
+++ legal/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.test.client import Client
 
-# Create your tests here.
+class TestLegalPages(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_can_visit_privacy(self):
+        response = self.client.get(reverse('legal:privacy'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_visit_terms_of_use(self):
+        response = self.client.get(reverse('legal:terms_of_use'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_visit_terms_of_conditions(self):
+        response = self.client.get(reverse('legal:terms_of_conditions'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_visit_about_us(self):
+        response = self.client.get(reverse('legal:about_us'))
+        self.assertEqual(response.status_code, 200)
