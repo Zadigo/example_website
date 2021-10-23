2a6efd3ac6e1c3135ca51bb7bb88271ba1ef49d5
Zadigo <pendenquejohn@gmail.com>
2021-10-23 22:23:27 +0200

    Preparing stable version

--- example_website/settings.py
+++ example_website/settings.py
@@ -1,9 +1,8 @@
-import os
 import datetime
+import os
 
 import stripe
 from django.utils.translation import gettext_lazy as _
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,13 +41,9 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'social_django',
     'django_filters',
-    # 'graphene_django',
-    # 'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
-    # 'graphql_auth',
 
     'company',
     'analytics',
-    'dashboard',
     'accounts',
     'legal',
     'hero',
@@ -88,7 +83,6 @@ TEMPLATES = [
 
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
-
                 'legal.context_processors.legal',
                 'company.context_processors.company',
                 'hero.context_processors.hero'
@@ -96,7 +90,6 @@ TEMPLATES = [
             'libraries': {
                 'utils': 'accounts.templatetags.utils',
                 'nodes_plus': 'nodesplus.templatetags.nodes_plus',
-
                 'analytics_tags': 'analytics.templatetags.analytics_tags'
             }
         },
@@ -172,99 +165,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'accounts.MyUser'
 
 AUTHENTICATION_BACKENDS = [
-    # 'graphql_auth.backends.GraphQLAuthBackend',
-    # 'graphql_jwt.backends.JSONWebTokenBackend',
-    # 'social_core.backends.twitter.TwitterOAuth',
-    # 'social_core.backends.open_id.OpenIdAuth',
-    # 'social_core.backends.google.GoogleOpenId',
-    # 'social_core.backends.google.GoogleOAuth2',
-    # 'social_core.backends.facebook.FacebookOAuth2',
     'accounts.backends.EmailAuthenticationBackend'
 ]
 
-
-# Social Django
-
-LOGIN_URL = 'accounts:login'
-
-LOGOUT_URL = 'accounts:logout'
-
-LOGIN_REDIRECT_URL = 'accounts:profile:home'
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
-
-
-# LOGGING
-
-# LOGGING = {
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'werkzeug': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
-
-
-# GMAIL
-
-EMAIL_HOST = 'smtp.gmail.com'
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
-
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
-
-EMAIL_USE_TLS = True
-
-EMAIL_PORT = 587
-
-EMAIL_USE_LOCALTIME = True
-
-
-# AMAZON S3
-
-# AWS_ACCESS_KEY_ID = ''
-
-# AWS_SECRET_ACCESS_KEY = ''
-
-# AWS_STORAGE_BUCKET_NAME = ''
-
-# AWS_S3_REGION_NAME = ''
-
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400'
-# }
-
-# AWS_QUERYSTRING_AUTH = False
-
-# AWS_DEFAULT_ACL = None
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# AWS_LOCATION = 'nawoka/static'
-
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-
-# STATIC_ROOT = 'staticfiles'
-
-# AWS_MEDIA_LOCATION = ''
-
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Debugging
 
@@ -299,25 +202,45 @@ LANGUAGES = [
 ]
 
 
-# Stripe
-
-STRIPE_TEST_KEYS = []
-
-STRIPE_LIVE_KEYS = []
-
-try:
-    if DEBUG:
-        stripe.api_key = STRIPE_TEST_KEYS[0]
-    else:
-        stripe.api_key = STRIPE_LIVE_KEYS[0]
-except:
-    pass
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# HERO
+# CORS
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r'^https?\:\/\/localhost\:808\d{1}$',
+    r'^https?\:\/\/192\.168\.0\.\d{3}\:8080$'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'localhost:8080',
+    '192.168.0.103:8080'
+]
+
+
+# Rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ['Bearer', 'Token'],
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'api.utils.jwt_response_payload_handler',
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1)
+}
+
+
+# Other applications
 
 HERO = {
     'hero_image': 'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg'
@@ -346,82 +269,29 @@ ENTERPRISE = {
 }
 
 
-# CORS
+# Social Django
 
-CORS_ORIGIN_ALLOW_ALL = False
+LOGIN_URL = 'accounts:login'
 
-CORS_ALLOW_CREDENTIALS = True
+LOGOUT_URL = 'accounts:logout'
 
-CORS_ORIGIN_REGEX_WHITELIST = [
-    r'^https?\:\/\/localhost\:808\d{1}$',
-    r'^https?\:\/\/192\.168\.0\.\d{3}\:8080$'
-]
+LOGIN_REDIRECT_URL = 'accounts:profile:home'
 
-CSRF_TRUSTED_ORIGINS = [
-    'localhost:8080',
-    '192.168.0.103:8080',
-]
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 
-# GraphQL
-
-# GRAPHQL_AUTH = {
-#     'LOGIN_ALLOWED_FIELDS': 'email',
-#     'UPDATE_MUTATION_FIELDS': ['firstname', 'lastname'],
-#     'SEND_ACTIVATION_EMAIL': False,
-#     'REGISTER_MUTATION_FIELDS': {
-#         'email': 'String'
-#     }
-# }
-
-# GRAPHQL_JWT = {
-#     # 'JWT_PAYLOAD_HANDLER': 'app.utils.jwt_payload',
-#     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-#     'JWT_VERIFY_EXPIRATION': True,
-#     'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
-#     'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=5),
-#     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-#     'JWT_SECRET_KEY': os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY),
-#     'JWT_ALGORITHM': 'HS256',
-#     'JWT_ALLOW_ANY_CLASSES': [
-#         'graphql_auth.mutations.Register',
-#         # 'graphql_auth.mutations.Register',
-#         # 'graphql_auth.mutations.VerifyAccount',
-#         # 'graphql_auth.mutations.ResendActivationEmail',
-#         # 'graphql_auth.mutations.SendPasswordResetEmail',
-#         # 'graphql_auth.mutations.PasswordReset',
-#         # 'graphql_auth.mutations.ObtainJSONWebToken',
-#         # 'graphql_auth.mutations.VerifyToken',
-#         # 'graphql_auth.mutations.RefreshToken',
-#         # 'graphql_auth.mutations.RevokeToken',
-#         # 'graphql_auth.mutations.VerifySecondaryEmail'
-#     ]
-# }
-
-# GRAPHENE = {
-#     'SCHEMA': 'graph.schema.schema',
-#     'ATOMIC_MUTATIONS': True,
-#     'MIDDLEWARE': {
-#         'graphql_jwt.middleware.JSONWebTokenMiddleware'
-#     }
-# }
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 
 
-# Rest framework
+# Emailing
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-}
+EMAIL_HOST = 'smtp.gmail.com'
 
-SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ['Bearer', 'Token'],
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
-    'JWT_RESPONSE_PAYLOAD_HANDLER': 'api.utils.jwt_response_payload_handler',
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
-    # 'JWT_ALLOW_REFRESH': True,
-    # 'JWT_AUTH_HEADER_PREFIX': 'JWT'
-    # 'JWT_AUTH_HEADER_PREFIX': 'Token',
-}
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
+
+EMAIL_USE_TLS = True
+
+EMAIL_PORT = 587
+
+EMAIL_USE_LOCALTIME = True
